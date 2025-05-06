72514231f48480d4f9a5be876682ba97ba8a8c65
erodrigufer <erodrigufer@gmail.com>
2025-05-06 18:25:48 +0200

    debt: Remove left-over comments

--- backend/internal/server/handlers/tasks.go
+++ backend/internal/server/handlers/tasks.go
@@ -11,20 +11,6 @@ import (
 )
 
 func (h *Handlers) GetTasks() http.HandlerFunc {
-	// task1 := tasks.Task{Priority: "B", Description: "Buy a new notebook"}
-	//
-	// task6 := tasks.Task{Priority: "C", Description: "Find a new スマホ"}
-	// task2 := tasks.Task{Priority: "A", Description: "Insurance claim", Contexts: []string{"5分", "メール"}}
-	//
-	// task3 := tasks.Task{Completed: true, Description: "Send gift to X", Contexts: []string{"仕事"}}
-	// task4 := tasks.Task{Description: "Implement MVP backend template renderer", Contexts: []string{"programming"}, Projects: []string{"serenitynow"}}
-	//
-	// task5 := tasks.Task{Priority: "B", Description: "Buy train ticket", Contexts: []string{"Conference25", "仕事"}}
-	//
-	// tasks := []tasks.Task{task1, task2, task3, task4, task5, task6}
-	//
-	// tasks = sortTasksByPriority(tasks)
-
 	return func(w http.ResponseWriter, r *http.Request) {
 		tasks, err := h.sm.GetAllTasks(r.Context())
 		if err != nil {
@@ -38,21 +24,6 @@ func (h *Handlers) GetTasks() http.HandlerFunc {
 		}
 	}
 }
-
-// func sortTasksByPriority(ts []tasks.Task) []tasks.Task {
-// 	cmpFn := func(a, b tasks.Task) int {
-// 		// Tasks with no Priority field should be sorted to the bottom.
-// 		if a.Priority == "" {
-// 			return 1
-// 		}
-// 		if b.Priority == "" {
-// 			return -1
-// 		}
-// 		return cmp.Compare(a.Priority, b.Priority)
-// 	}
-// 	slices.SortStableFunc(ts, cmpFn)
-// 	return ts
-// }
 
 func (h *Handlers) PostTasks() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
