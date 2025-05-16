4150aa96d053b34b60c3a74b953cf6d034daf380
erodrigufer <erodrigufer@gmail.com>
2025-05-16 09:22:46 +0200

    UX: Tasks are ordered by due date as well
    - Most urgent tasks are shown at the top.
    - Tasks without due date are shown after tasks with due dates.

--- backend/internal/state/getAllTasks.sql
+++ backend/internal/state/getAllTasks.sql
@@ -1,3 +1,3 @@
 SELECT id, completed, priority, description, created_at, completed_at, due_at
   FROM tasks
-  ORDER BY completed, priority, description;
+  ORDER BY completed, priority, due_at ASC NULLS LAST, description;
